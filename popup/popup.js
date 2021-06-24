778894f669012c1f1fa8ede0bae6f36594f908d5
IdanS <idans2468@gmail.com>
2021-06-24 19:23:36 +0300

    delete minified code running and run the file instead

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,5 @@
-"use strict";
+"use strict"
 
-// minified courseRemover.js to be chrome compatible.
-const cr = '!async function(){if(window.hasRun)return await e(),void(window.hasRun=!1);async function e(){for(btns=document.getElementsByClassName("btn-close");0!==btns.length;){for(let e=0;e<btns.length;++e)btns[e].remove();btns=document.getElementsByClassName("btn-close")}}window.hasRun=!0,async function t(){var n=document.getElementsByClassName("type_course depth_3 contains_branch");for(let s=0;s<n.length-1;++s){let o=document.createElement("button");o.type="button",o.id="bruh"+s,o.className="btn-close",o.addEventListener("click",async function(){const o=n[s].querySelector("a[href]").getAttribute("href"),a=new URL(o).searchParams.get("id");saveToStorage("RemovedCourses",a,!1),await e(),n[s].remove(),t()}),n[s].insertBefore(o,n[s].firstChild)}}()}();'
 
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
@@ -14,44 +12,43 @@ async function listenForClicks() {
          */
         async function actionToScript(id) {
             switch (id) {
-                // TODO: maybe implement your own css or take the class you need from bootstrap for buttons instead of injecting bootstrap css to the page
                 case "remove-courses":
-                    browser.tabs.executeScript({code: cr}).catch(reportError);
+                    browser.tabs.executeScript({file: '/content_scripts/courseRemover.js'}).catch(reportError)
                     sendMessageToTabs("CourseRemoverStatus", {val: !window.courseRemoverStatus}).catch(reportError)
-                    return;
+                    return
                 case "dark-mode":
                     sendMessageToTabs("DarkMode", {val: !window.darkMode}).catch(reportError)
-                    return;
+                    return
                 case "enhance-page":
                     location.href = "./enahnceMenu.html"
-                    return;
+                    return
                 case "reset":
-                    await handleReset().catch(reportError);
-                    return;
+                    await handleReset().catch(reportError)
+                    return
                 case "monochrome":
-                    sendMessageToTabs("MonoChrome", {val: !window.monochrome}).catch(reportError);
-                    return;
+                    sendMessageToTabs("MonoChrome", {val: !window.monochrome}).catch(reportError)
+                    return
                 case "cursor":
                     const val = window.cursor === "big" ? "normal" : "big"
-                    sendMessageToTabs("EnhancePage", {cursor: val}).catch(reportError);
-                    return;
+                    sendMessageToTabs("EnhancePage", {cursor: val}).catch(reportError)
+                    return
                 case "back":
                     location.href = './index.html'
-                    return;
+                    return
             }
         }
 
         if (e.target.classList.contains("btn")) {
-            actionToScript(e.target.id);
+            actionToScript(e.target.id)
         }
-    });
+    })
 }
 
 async function handleReset() {
     const confirm = window.confirm("Are you sure? reset is an irreversible action")
     if (confirm) {
         sendMessageToTabs("reset")
-        alert("Please refresh your browser tab to apply.");
+        alert("Please refresh your browser tab to apply.")
     }
 }
 
@@ -59,7 +56,7 @@ async function handleReset() {
  * Just log the error to the console.
  */
 function reportError(error) {
-    console.log("Something went wrong " + error);
+    console.log("Something went wrong " + error)
 }
 
 function listenForRange() {
@@ -91,7 +88,7 @@ async function sendMessageToTabs(action, payload = {}) {
     const [tab] = await browser.tabs.query({
         currentWindow: true,
         active: true
-    }).catch(onError);
+    }).catch(onError)
 
     const res = await browser.tabs.sendMessage(
         tab.id,
@@ -101,10 +98,10 @@ async function sendMessageToTabs(action, payload = {}) {
         }
     )
     setBtnsStyle(res)
-    window.courseRemoverStatus = res.courseRemoverStatus;
-    window.darkMode = res.DarkMode;
-    window.monochrome = res.EnhancePage.Monochrome;
-    window.cursor = res.EnhancePage.cursor;
+    window.courseRemoverStatus = res.courseRemoverStatus
+    window.darkMode = res.DarkMode
+    window.monochrome = res.EnhancePage.Monochrome
+    window.cursor = res.EnhancePage.cursor
     return res
 }
 
@@ -123,13 +120,13 @@ const setBtnsStyle = (localStorageData) => {
 }
 
 function setBtnStyle(btnID, isActive) {
-    const NOT_ACTIVE = "menu-item btn";
-    const ACTIVE = "menu-item btn active";
+    const NOT_ACTIVE = "menu-item btn"
+    const ACTIVE = "menu-item btn active"
     isActive ? document.getElementById(btnID).className = ACTIVE : document.getElementById(btnID).className = NOT_ACTIVE
 }
 
 function setSliderValue(sliderID, value) {
-    document.getElementById(sliderID).value = value;
+    document.getElementById(sliderID).value = value
 }
 
 function onError(error) {
@@ -137,10 +134,10 @@ function onError(error) {
 }
 
 async function loader() {
-    listenForClicks();
-    listenForRange();
-    sendMessageToTabs("");
+    listenForClicks()
+    listenForRange()
+    sendMessageToTabs("")
 }
 
 
-loader();
+loader()
