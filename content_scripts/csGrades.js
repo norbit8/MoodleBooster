4dbc3cf83e5e1c06b813ffecd0a9b5464768d631
liron menda <lironmendaq@gmail.com>
2021-06-26 19:41:12 +0300

    Added cs grades and spinner support

---
+++ content_scripts/csGrades.js
@@ -0,0 +1,111 @@
+function addGradesButton() {
+    if (isCsGradesAvailable() && !isButtonExist()) { //Only if exists grades in cs admin add those
+        const dashboard = window.moodleBoosterDashboard;
+        initCsGradesButton(dashboard);
+    }
+
+}
+function isButtonExist() {
+    return getElementById('gradesBtn') !== null;
+
+}
+function isCsGradesAvailable() {
+    return !!document.getElementsByClassName("block_course_admin")[0];
+}
+function initCsGradesButton(dashboard) {
+    dashboard.insertHtmlToDashboard('<button id="gradesBtn" class="menu-button">Show CS Admin Grades</button>', dashboard.getButtonStyle());
+    document.getElementById("gradesBtn").addEventListener("click", (e) => onShowGradesClicked(e, dashboard));
+}
+function parseCommentsFromCsAdmin(csHtml, tableRow) {
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(csHtml, 'text/html');
+    var data = htmlDoc.getElementsByTagName('ul');
+    if (!!data) {
+        tableRow.push(`<td><ul>${htmlDoc.getElementsByTagName('ul')[0].innerHTML}</ul></td>`);
+    }
+    else {
+        tableRow.push('<td></td>')
+    }
+
+}
+async function extractDataFromTable(table) {
+    let tableRows = [];
+
+    //Build the table of grades :
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        let tableRow = [];
+        for (var j = 0, col; col = row.cells[j]; j++) {
+            if (j === 0 || j === 3) {
+                continue; //empty col or date col
+            }
+            tableRow.push(`<td>${col.innerText}</td>`);
+            if (j === 2) //Link to the comments 
+            {
+                const link = col.querySelector('a')
+                if (!link) {
+                    continue;
+                }
+                const url = link.href;
+                if (!!url) {
+                    await fetch(url).then(function (response) {
+                        // When the page is loaded convert it to text
+                        return response.text()
+                    })
+                        .then((html) => parseCommentsFromCsAdmin(html, tableRow)
+                        );
+                }
+            }
+        }
+        tableRows.push(`<tr>${tableRow.join().replaceAll(',', '')}</tr>`);
+    }
+    tableRows = tableRows.join().replaceAll(',', '');
+    return tableRows;
+}
+
+async function onShowGradesClicked(e, dashboard) {
+    if (!!document.getElementById('gradesTable') || !!document.getElementById('loadingSpinner')) {
+        return;
+    }
+    dashboard.addSpinner();
+
+    const section = document.getElementsByClassName("block_course_admin")[0];
+    if (!section) {
+        return; //No cs grades
+    }
+    const table = section.querySelector('table');
+    const tableRows = await extractDataFromTable(table);
+
+    let template = getTemplate(tableRows);
+    let topDiv = document.getElementsByClassName("course-content")[0];
+    var gradesDiv = document.createElement("div");
+    gradesDiv.className = ''
+    gradesDiv.innerHTML = template;
+    gradesDiv.id = 'gradesTable';
+    topDiv.insertBefore(gradesDiv, topDiv.childNodes[0]);
+    dashboard.removeSpinner();
+};
+function getTemplate(tableRows) {
+    return `
+     <h5>ציונים</h5>
+     <table class="scheduale-table">
+         <thead>
+             <tr>
+                 <th style="width: 33%;"> שם </th>
+                 <th style="width: 33%;">ציון </th>
+                 <th style="width: 34%;"> הערות </th>
+             </tr>
+         </thead>
+         <tbody>
+             ${tableRows}
+         </tbody>
+     </table>
+     `;
+}
+
+
+
+if (isCsGradesAvailable()) { //Only if exists grades in cs admin add those
+    initCsGradesButton(dashboard);
+}
+addGradesButton()
+
--- content_scripts/dashboard.js
+++ content_scripts/dashboard.js
@@ -1,19 +1,27 @@
-class Dashboard{
-    constructor(){
+class Dashboard {
+    constructor() {
         this.container = this.createContainer()
     }
-    createContainer(){
-        try{
+
+    createContainer() {
+        try {
+            const prevContainer = document.querySelector('.dashboard-container')
+            if (prevContainer !== null) {
+                prevContainer.remove()
+            }
             const main = document.querySelector('[id="region-main-box"]')
             var moodleBoosterContainer = document.createElement("div");
+            moodleBoosterContainer.id = 'dashboard-container';
             moodleBoosterContainer.className = 'dashboard-container'
-            main.insertBefore(moodleBoosterContainer,main.firstChild)
+            main.insertBefore(moodleBoosterContainer, main.firstChild)
             this.addDashboardContainerStyle()
+            this.addSpinnerStyle();
             return moodleBoosterContainer;
-        }catch(err){
+        } catch (err) {
             return null;
         }
     }
+
     /**
      * Utility function to add CSS in multiple passes.
      * @param {string} styleString
@@ -23,6 +31,66 @@ class Dashboard{
         style.textContent = styleString;
         document.head.append(style);
     }
+
+    addSpinner() {
+        if (!!document.getElementById('spinner')) {
+            return;
+        }
+        var spinnerDiv = document.createElement('div');
+        spinnerDiv.className = 'loader';
+        spinnerDiv.id = 'spinner';
+        var board = document.getElementById('dashboard-container');
+        if (!!board) {
+            board.appendChild(spinnerDiv);
+        }
+    }
+
+    removeSpinner() {
+        document.getElementById('spinner').outerHTML = "";
+    }
+    getButtonStyle() {
+        return `
+        .menu-button{
+            background-color: #269CE9;
+            border: none;
+            color: white;
+            padding: 6px 28px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            margin: 5px;
+            font-size: 14px;
+            border-radius: 5px;
+            font-weight: bold;
+          }
+          
+        .menu-button:hover{
+            background-color: #70B9E8;
+        }
+        
+        .menu-button:active{
+            background: #269CE9;
+        }
+        `
+    };
+
+    addSpinnerStyle() {
+        this.addStyle(`
+        .loader {
+            border: 8px solid #f3f3f3; /* Light grey */
+            border-top: 8px solid #3498db; /* Blue */
+            border-radius: 50%;
+            width: 60px;
+            height: 60px;
+            animation: spin 2s linear infinite;
+          }
+          
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }`)
+    }
+
     addDashboardContainerStyle() {
         this.addStyle(`
         .dashboard-container{
@@ -37,16 +105,37 @@ class Dashboard{
             background-color: #CCF2FF;
         }
         `);
+
     }
-    insertHtmlToDashboard(strictHtml,style){
-        try{
-            this.container.insertAdjacentHTML('beforeend',strictHtml );
+
+    insertHtmlToDashboard(strictHtml, style, where = 'beforeend') {
+        try {
+            if (typeof strictHtml === "string") {
+                this.container.insertAdjacentHTML(where, strictHtml);
+            } else {
+                this.container.insertAdjacentElement(where, strictHtml)
+            }
             this.addStyle(style);
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
 }
-const dashboard = new Dashboard()
-dashboard.insertHtmlToDashboard('<h1 class="title">MoodleBooster Dashboard</h1>',`.title{font-family:Arial; margin-bottom:15px;}`)
+
+function isInCourseView() {
+    let url = new URL(window.location.href)
+    return url.searchParams.get("id") !== null && url.pathname.includes("course/view.php")
+}
+
+async function createDashboard() {
+    const dashboard = new Dashboard()
+    window.moodleBoosterDashboard = dashboard
+    dashboard.insertHtmlToDashboard('<h1 class="title">MoodleBooster Dashboard</h1>', `.title{font-family:Arial; margin-bottom:15px;}`)
+
+}
+
+
+if (isInCourseView()) {
+    createDashboard()
+}
 //TODO add more html content with dashboard.insertHtmlToDashboard()
